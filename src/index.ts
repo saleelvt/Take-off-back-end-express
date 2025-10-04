842923803efdc2d6a4de6d01930465e2079b29ef
saleel vt <saleelvt57@gmail.com>
2025-10-04 08:00:47 +0530

    admin insert removed

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,32 @@ import app from "@/presentation/server";
 dotenv.config(); // Load environment variables
 
 
+
+// Function to initialize an admin (example)
+// export const insertAdmin = async () => {
+//   const sampleAdmin = {
+//     userName: "takeoff",
+//     email: "takeoffcaravan@gmail.com",
+//     password: "admin@takeoff",
+//   };
+//   // Insert logic for saving admin to the database if required here
+
+//   try {
+//   const existingAdmin = await (Admin as mongoose.Model<AdminEntity>).findOne({ 
+//   email: sampleAdmin.email 
+// });
+//     if (!existingAdmin) {
+//       const newAdmin = new Admin(sampleAdmin);
+//       await newAdmin.save();
+//       console.log("this is the admin now ", newAdmin);
+//     } else {
+//       console.log(" ADMIN ALLREDY EXISTED  ");
+//     }
+//   }  catch (error) {
+//     console.error("Failed to insert sample admin:", error);
+//   }
+// };
+
 (async () => {
   try {
     console.log("Initializing server and database connection...");
